e54e7a59840aa10215bed898ea10c73cb335cf42
laradreamer79 <laradreamer79@gmail.com>
2026-02-18 10:38:38 +0300

    Add Places, Amenities, and Reviews namespaces

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -8,6 +8,13 @@ def create_app():
     api.init_app(app)
 
     from app.api.v1.users import ns as users_ns
+    from app.api.v1.places import ns as places_ns
+    from app.api.v1.amenities import ns as amenities_ns
+    from app.api.v1.reviews import ns as reviews_ns
+    
     api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
 
     return app
